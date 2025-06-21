ec71efc559f15309bbaba118414cfc8970fb6b67
Kirill Zlobin <jsdevzk@gmail.com>
2025-06-21 11:04:12 +0200

    Continued...

--- src/components/Pieces/Bishop/Bishop.tsx
+++ src/components/Pieces/Bishop/Bishop.tsx
@@ -7,7 +7,7 @@ const Bishop = ({ coloredChessPiece, position }: ChessPieceProps) => {
   const Bishop = color === "White" ? WhiteBishop : BlackBishop;
 
   const onClick = () => {
-    alert(`Pawn clicked at position: ${position}`);
+    console.log(`Pawn clicked at position: ${position}`);
   };
   return <img src={Bishop} alt="Black Bishop" style={{ width: "100%", height: "100%" }} onClick={onClick} />;
 };
--- src/components/Pieces/Knight/Knight.tsx
+++ src/components/Pieces/Knight/Knight.tsx
@@ -7,7 +7,7 @@ const Knight = ({ coloredChessPiece, position }: ChessPieceProps) => {
 
   const Knight = color === "White" ? WhiteKnight : BlackKnight;
   const onClick = () => {
-    alert(`Pawn clicked at position: ${position}`);
+    console.log(`Pawn clicked at position: ${position}`);
   };
 
   return <img src={Knight} alt="Black Pawn" style={{ width: "100%", height: "100%" }} onClick={onClick} />;
