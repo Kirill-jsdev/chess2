69a211051e3cd315ef7fee1dab4b375c34f8561e
Kirill Zlobin <jsdevzk@gmail.com>
2025-06-21 10:42:07 +0200

    Continued...

--- src/store/slices/chessboardSlice.ts
+++ src/store/slices/chessboardSlice.ts
@@ -2,14 +2,21 @@ import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import { STARTING_POSITION } from "../../utils/startingPosition";
 import type { ChessPieceColored, PieceColor, Position } from "../../components/types/types";
 
+type PieceOnBoard = {
+  piece: ChessPieceColored;
+  position: Position;
+};
+
 type ChessboardSlice = {
   startingPosition: Record<Position, ChessPieceColored>;
   currentTurn: PieceColor;
+  selectedPiece: PieceOnBoard | null;
 };
 
 const initialState: ChessboardSlice = {
   startingPosition: STARTING_POSITION,
   currentTurn: "White",
+  selectedPiece: null,
 };
 
 export const chessboardSlice = createSlice({
@@ -21,6 +28,31 @@ export const chessboardSlice = createSlice({
       const piece = state.startingPosition[oldPosition];
       delete state.startingPosition[oldPosition];
       state.startingPosition[newPosition] = piece;
+    },
+    select: (state, action: PayloadAction<Position>) => {
+      const position = action.payload;
+
+      // Check if the position has a piece
+      if (!state.startingPosition[position]) {
+        return state;
+      }
+
+      // Check if it's the player's turn
+      const color = state.startingPosition[position].split("-")[1] as PieceColor;
+      if (color !== state.currentTurn) {
+        return state; // Not the player's turn
+      }
+
+      // If a piece is already selected, deselect it
+      if (state.selectedPiece && state.selectedPiece.position === position) {
+        state.selectedPiece = null;
+        return state;
+      }
+
+      state.selectedPiece = {
+        piece: state.startingPosition[position],
+        position: position,
+      };
     },
   },
 });
