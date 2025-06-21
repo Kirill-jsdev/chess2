c80eb23efafc42c473f87f5d6b49e4cfc4128bb3
Kirill Zlobin <jsdevzk@gmail.com>
2025-06-21 17:47:19 +0200

    Added Queen moves

--- src/components/Pieces/Queen/Queen.tsx
+++ src/components/Pieces/Queen/Queen.tsx
@@ -1,16 +1,61 @@
 import BlackQueen from "../../../assets/Bqueen.svg";
 import WhiteQueen from "../../../assets/Wqueen.svg";
-import type { ChessPieceProps } from "../../types/types";
+import { useAppDispatch, useAppSelector } from "../../../store";
+import { select } from "../../../store/slices/chessboardSlice";
+import type { BoardState, ChessPieceProps, Position } from "../../types/types";
 
 const Queen = ({ coloredChessPiece, position }: ChessPieceProps) => {
+  const dispatch = useAppDispatch();
+  const board = useAppSelector((state) => state.chessboard.board);
   const [, color] = coloredChessPiece!.split("-") || [];
 
   const Queen = color === "White" ? WhiteQueen : BlackQueen;
   const onClick = () => {
-    console.log("Queen clicked", position);
+    dispatch(select({ position, availableMoves: getQueenMoves(position, color as "White" | "Black", board) }));
   };
 
   return <img src={Queen} alt="Black Pawn" style={{ width: "100%", height: "100%" }} onClick={onClick} />;
 };
 
 export default Queen;
+
+//Helper functions
+
+function getQueenMoves(position: Position, color: "White" | "Black", board: BoardState): Position[] {
+  const file = position[0]; // 'a' to 'h'
+  const rank = parseInt(position[1]); // 1 to 8
+
+  const moves: Position[] = [];
+  const directions = [
+    [1, 0], // right
+    [-1, 0], // left
+    [0, 1], // up
+    [0, -1], // down
+    [1, 1], // up-right
+    [1, -1], // down-right
+    [-1, 1], // up-left
+    [-1, -1], // down-left
+  ];
+
+  for (const [df, dr] of directions) {
+    let step = 1;
+    while (true) {
+      const newFileCode = file.charCodeAt(0) + df * step;
+      const newRank = rank + dr * step;
+      if (newFileCode < 97 || newFileCode > 104 || newRank < 1 || newRank > 8) break;
+      const newPos = `${String.fromCharCode(newFileCode)}${newRank}` as Position;
+      const target = board[newPos];
+      if (!target) {
+        moves.push(newPos);
+      } else {
+        if (target.split("-")[1] !== color) {
+          moves.push(newPos);
+        }
+        break; // Can't jump over pieces
+      }
+      step++;
+    }
+  }
+
+  return moves;
+}
