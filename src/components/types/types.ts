81ca978033456070cd01b8dd77b09162a77b0c88
Kirill Zlobin <jsdevzk@gmail.com>
2025-06-21 11:57:34 +0200

    Continued...

--- src/components/types/types.ts
+++ src/components/types/types.ts
@@ -1,7 +1,10 @@
 export type PieceColor = "White" | "Black";
 export type ChessPieceName = "King" | "Queen" | "Rook" | "Bishop" | "Knight" | "Pawn";
 export type ChessPieceColored = `${ChessPieceName}-${PieceColor}`;
-export type Position = `${"a" | "b" | "c" | "d" | "e" | "f" | "g" | "h"}${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8}`;
+
+export type File = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h";
+export type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+export type Position = `${File}${Rank}`;
 
 export type ChessPieceProps = {
   coloredChessPiece?: ChessPieceColored;
