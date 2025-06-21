897f38bd92343a08088e137ea3706ced1a4f9340
Kirill Zlobin <jsdevzk@gmail.com>
2025-06-21 10:06:42 +0200

    Continued...

--- src/components/Pieces/Pawn/Pawn.tsx
+++ src/components/Pieces/Pawn/Pawn.tsx
@@ -1,14 +1,17 @@
 import BlackPawn from "../../../assets/Bpawn.svg";
 import WhitePawn from "../../../assets/Wpawn.svg";
+import { useAppDispatch } from "../../../store";
+import { move } from "../../../store/slices/chessboardSlice";
 import type { ChessPieceProps } from "../../types/types";
 
 const Pawn = ({ coloredChessPiece, position }: ChessPieceProps) => {
+  const dispatch = useAppDispatch();
   const [, color] = coloredChessPiece!.split("-") || [];
 
   const Pawn = color === "White" ? WhitePawn : BlackPawn;
 
   const onClick = () => {
-    alert(`Pawn clicked at position: ${position}`);
+    dispatch(move({ oldPosition: position, newPosition: "e4" }));
   };
 
   return <img src={Pawn} alt="Black Pawn" style={{ width: "100%", height: "100%" }} onClick={onClick} />;
