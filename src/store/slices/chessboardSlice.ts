a1ac47e2a84a30ce66e1b8360b23c60b5d898e85
Kirill Zlobin <jsdevzk@gmail.com>
2025-06-21 10:02:24 +0200

    Continued...

--- src/store/slices/chessboardSlice.ts
+++ src/store/slices/chessboardSlice.ts
@@ -1,7 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { Position } from "../../components/Square/Square";
-import type { ChessPieceColored } from "../../components/ChessPiece/ChessPiece";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import { STARTING_POSITION } from "../../utils/startingPosition";
+import type { ChessPieceColored, Position } from "../../components/types/types";
 
 type ChessboardSlice = {
   startingPosition: Record<Position, ChessPieceColored>;
@@ -14,5 +13,14 @@ const initialState: ChessboardSlice = {
 export const chessboardSlice = createSlice({
   name: "chessboard",
   initialState,
-  reducers: {},
+  reducers: {
+    move: (state, action: PayloadAction<{ oldPosition: Position; newPosition: Position }>) => {
+      const { oldPosition, newPosition } = action.payload;
+      const piece = state.startingPosition[oldPosition];
+      delete state.startingPosition[oldPosition];
+      state.startingPosition[newPosition] = piece;
+    },
+  },
 });
+
+export const { move } = chessboardSlice.actions;
