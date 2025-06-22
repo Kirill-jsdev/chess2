31321614062825e39a888d7ada22e6ab74d29902
Kirill Zlobin <jsdevzk@gmail.com>
2025-06-22 08:39:54 +0200

    Added temporary check message

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import { useAppSelector } from "./store";
 
 function App() {
   const board = useAppSelector((store) => store.chessboard.board);
+  const isCheck = useAppSelector((store) => store.chessboard.isCheck);
 
-  return <Chessboard board={board} squareSize="30px" />;
+  return (
+    <div style={{ display: "flex" }}>
+      <Chessboard board={board} squareSize="30px" />
+      {isCheck && <div>CHECK!!!</div>}
+    </div>
+  );
 }
 
 export default App;
