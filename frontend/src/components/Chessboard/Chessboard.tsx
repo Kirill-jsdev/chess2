58e3b74eaa266398daa98d6d61f6a282206db2ff
Kirill Zlobin <jsdevzk@gmail.com>
2025-06-26 23:10:30 +0200

    Added orientation property

--- frontend/src/components/Chessboard/Chessboard.tsx
+++ frontend/src/components/Chessboard/Chessboard.tsx
@@ -1,16 +1,24 @@
 import Square from "../Square/Square";
-import type { BoardState, Position } from "../types/types";
+import type { BoardState, Color, Position } from "../types/types";
 
 type ChessboardProps = {
   board: BoardState;
+  orientation: Color;
   squareSize?: string; //in css units, e.g., "25px"
 };
 
-const Chessboard = ({ board, squareSize = "25px" }: ChessboardProps) => {
+const Chessboard = ({ board, orientation, squareSize = "50px" }: ChessboardProps) => {
   const squares = [];
   for (let i = 0; i < 64; i++) {
-    const row = Math.floor(i / 8);
-    const col = i % 8;
+    // Calculate row and col based on orientation
+    let row = Math.floor(i / 8);
+    let col = i % 8;
+
+    if (orientation === "Black") {
+      row = 7 - row;
+      col = 7 - col;
+    }
+
     const isBlack = (row + col) % 2 === 1;
     const position = `${String.fromCharCode(97 + col)}${8 - row}` as Position;
     squares.push(<Square key={i} color={isBlack ? "#739552" : "#ebecd0"} position={position} chessPiece={board[position]} />);
